c5af097753ba84baacf7ce7441a1f1bd95fa22d5
JuanDavid24 <31551453+JuanDavid24@users.noreply.github.com>
2025-06-06 16:55:02 -0300

    Refac: more scalable request building + better error handling

--- index.js
+++ index.js
@@ -5,18 +5,35 @@ if (args.length < 2)
   console.error("No se ingresaron los parámetros suficientes");
 else {
   const [method, path, ...productData] = args;
-  let config = {
-    method,
-    path,
-    headers: { "Content-Type": "application/json" },
+
+  const reqSettings = {
+    GET: {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    },
+    POST: {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        title: productData[0],
+        price: productData[1],
+        category: productData[2],
+      }),
+    },
+    DELETE: {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    },
   };
 
-  if (method === "POST" && productData) {
-    config.body = JSON.stringify({
-      title: productData[0],
-      price: productData[1],
-      category: productData[2],
-    });
+  const config = reqSettings[method];
+
+  if (!config) {
+    console.error("Método ingresado no soportado");
+    process.exit(1);
   }
-  fetchData(`https://fakestoreapi.com/${path}`, config)
+
+  const data = await fetchData(`https://fakestoreapi.com/${path}`, config);
+
+  if (data) console.log(data);
 }
--- productsService.js
+++ productsService.js
@@ -1,8 +1,11 @@
 export const fetchData = async(URL, config) => {
   try {
     const res = await fetch(URL, config)
+    if (!res.ok) {
+      throw new Error(`El servicio respondio con código de error: ${res.status}`);
+    }
     const data = await res.json() 
-    console.log(data);
+    return data
   } catch (error) {
     console.error("No se pudo realizar la operación", error)
   }
